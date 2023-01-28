0ec8d6120585db1319c5681a41ae24f49381b081
Brandon Apol <brandonapol@cedarville.edu>
2023-01-28 12:58:17 -0500

    learned what I needed to, moving on

--- src/middleware/sessionAuth.ts
+++ src/middleware/sessionAuth.ts
@@ -1,28 +1,29 @@
+
+/* eslint-disable camelcase */
 import { ExpressOIDC } from "@okta/oidc-middleware";
-// global.d.ts 'declare module' will fix the error
 import session from "express-session";
 
 export const register = ( app: any ) => {
-    // Create the OIDC client
-    const oidc = new ExpressOIDC( {
-        client_id: process.env.OKTA_CLIENT_ID,
-        client_secret: process.env.OKTA_CLIENT_SECRET,
-        issuer: `${ process.env.OKTA_ORG_URL }/oauth2/default`,
-        redirect_uri: `${ process.env.HOST_URL }/authorization-code/callback`,
-        appBaseUrl:`${ process.env.HOST_URL }`,
-        scope: "openid profile"
-    } );
+	// Create the OIDC client
+	const oidc = new ExpressOIDC( {
+		client_id: process.env.OKTA_CLIENT_ID,
+		client_secret: process.env.OKTA_CLIENT_SECRET,
+		issuer: `${ process.env.OKTA_ORG_URL }/oauth2/default`,
+		redirect_uri: `${ process.env.HOST_URL }/authorization-code/callback`,
+		appBaseUrl:`${ process.env.HOST_URL }`,
+		scope: "openid profile"
+	} );
 
-    // Configure Express to use authentication sessions
-    app.use( session( {
-        resave: true,
-        saveUninitialized: false,
-        secret: process.env.SESSION_SECRET
-    } ) );
+	// Configure Express to use authentication sessions
+	app.use( session( {
+		resave: true,
+		saveUninitialized: false,
+		secret: process.env.SESSION_SECRET
+	} ) );
 
-    // Configure Express to use the OIDC client router
-    app.use( oidc.router );
+	// Configure Express to use the OIDC client router
+	app.use( oidc.router );
 
-    // add the OIDC client to the app.locals
-    app.locals.oidc = oidc;
+	// add the OIDC client to the app.locals
+	app.locals.oidc = oidc;
 };
